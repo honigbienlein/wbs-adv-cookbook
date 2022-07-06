40c81c959b76d484d22e114ced7cd4fca67a231c
Neimante <neimante.railaite@gmail.com>
2022-07-06 14:38:29 +0200

    added footer

---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './index.css';
+
+function Footer () {
+
+    return (
+        <section className="footer">
+         <section className="footer-legalrequirements">
+            Legal Requirements
+         </section>    
+        </section>
+    )
+
+}
+
+
+export default Footer;
