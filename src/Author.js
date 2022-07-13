0cc5b91e9d8a76dec7f54228617fa982e9854ab9
Melis Augsburger <melis.augsburger@outlook.com>
2022-07-13 13:51:21 +0200

    author/s component finished

---
+++ src/Author.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Author(){
+// API fetch getAuthor
+
+    return(
+        <>
+            <p><strong>Here some facts about {author.nickName}</strong></p>
+            <div>
+                {/*{author.picture}*/}
+            </div>
+            <div>
+                {/* <p>Name: {author.nickName}</p>
+                <p>Birthday: {author.birthday}</p>
+                <p>Member since: {author.addDate}</p> */}
+            </div>
+        </>
+    )
+}
