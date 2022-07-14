d3e01ab1d1a5450800a814ec1fa4c24ff179d35b
Neimante <neimante.railaite@gmail.com>
2022-07-14 14:25:41 +0200

    added more components

--- src/Footer.js
+++ src/Footer.js
@@ -4,9 +4,13 @@ export default function Footer () {
 
     return (
         <footer className="footer">
+         <section >
+            <a href ="/" target =" _blank" rel="noopener norefferer">© Copyright 2022 Community Cookpook</a>
+         </section>       
          <section className="footer-legal-requirements">
             Legal Requirements
-         </section>    
+            <a href ="/" target =" _blank" rel="noopener norefferer">Legal Requirements</a>
+         </section>     
         </footer>
     )
 
