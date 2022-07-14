ee9053668e65e2e573e8f8b33bc1dc1595b8f1c0
Melis Augsburger <melis.augsburger@outlook.com>
2022-07-14 18:20:48 +0200

    functions of authors component

--- src/Authors.js
+++ src/Authors.js
@@ -1,19 +1,41 @@
 import React from 'react';
-
 export default function Authors(){
-// API fetch getAuthors
+
+    const contentful = require("contentful");
+    const client = contentful.createClient({
+    // This is the space ID. A space is like a project folder in Contentful terms
+    space: process.env.REACT_APP_SPACE_ID,
+    // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+    accessToken: process.env.REACT_APP_API_KEY
+    });
+    // This API call will request an entry with the specified ID from the space defined at the top, using a space-specific access token.
+    client
+    .getEntries()
+    .then( (res) =>{
+        //console.log(res);
+        const authors = res.items.filter((item) => {
+            if(item.fields.hasOwnProperty('nickname')) {
+                return item;
+            }
+        })
+        //console.log(authors);
+    }
+    )
+    .catch(err => console.log(err));    
 
     return(
         <>
             <p><strong>Our lovely cookers</strong></p>
-            <ul>
-               {/*  {authors.map(({ profilePicture, nickName }) => (
+            {/* <ul>
+                {authors.map((author) => (
                     <li>
-                        <img src={profilePicture} alt="profilePicture"/>
-                        <p>{nickName}</p>
+                        <img src={author.fields.profilePicture.fields.file.url} alt="profilePicture"/>
+                        <p>{author.fields.nickname}</p>
                     </li>
-                ))} */}
-            </ul>
+                ))} 
+            </ul> */}
         </>
     )
 }
+
+//usestate authors ternary operator
--- src/Search.js
+++ src/Search.js
@@ -4,16 +4,16 @@ import "./index.css";
 
 export default function Search(){
     
-    function search(){
+/*     function search(){
 
         // api suche search
-    }
+    } */
     
     
     return(
     
-        
-        <input className="Searchbar" type="text" value={search}></input>
+        ''
+        //<input className="Searchbar" type="text" value={search}></input>
   
     )
 }
