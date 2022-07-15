fd784f0c806d561229acd49692c640b4ee0259b6
Melis Augsburger <melis.augsburger@outlook.com>
2022-07-15 11:09:49 +0200

    start of useState

--- src/Authors.js
+++ src/Authors.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 export default function Authors(){
 
     const contentful = require("contentful");
@@ -9,31 +10,36 @@ export default function Authors(){
     accessToken: process.env.REACT_APP_API_KEY
     });
     // This API call will request an entry with the specified ID from the space defined at the top, using a space-specific access token.
+    const [authors, setAuthors] = useState([]);
     client
-    .getEntries()
-    .then( (res) =>{
-        //console.log(res);
-        const authors = res.items.filter((item) => {
-            if(item.fields.hasOwnProperty('nickname')) {
-                return item;
-            }
-        })
-        //console.log(authors);
-    }
-    )
+        .getEntries()
+        .then( (res) =>{
+            //console.log(res);
+            const authors = res.items.filter((item) => {
+                if(item.fields.hasOwnProperty('nickname')) {
+                    return setAuthors(item);
+                }
+            })
+        }
+        )
     .catch(err => console.log(err));    
+
+    console.log(authors);
 
     return(
         <>
             <p><strong>Our lovely cookers</strong></p>
-            {/* <ul>
-                {authors.map((author) => (
+             <ul>
+                {(authors.length > 0) ? 
+                authors.map((author) => (
                     <li>
-                        <img src={author.fields.profilePicture.fields.file.url} alt="profilePicture"/>
-                        <p>{author.fields.nickname}</p>
+                        {/* <img src={author.fields.profilePicture.fields.file.url} alt="profilePicture"/>
+                        <p>{author.fields.nickname}</p> */}
+                        <p>it works</p>
                     </li>
-                ))} 
-            </ul> */}
+                )) 
+                : ''}
+            </ul> 
         </>
     )
 }
