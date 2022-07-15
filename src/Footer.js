3ee837f911bfb646f9c4ca744f0598db3b2ca84f
Neimante <neimante.railaite@gmail.com>
2022-07-15 11:27:32 +0200

    -

--- src/Footer.js
+++ src/Footer.js
@@ -5,12 +5,23 @@ export default function Footer () {
 
     return (
         <footer className="footer">
+
           <div className="footer-link-legal">
           <Link to='/legal'>Legal</Link>
           </div>
+
+          <div className="footer-link-policy">
+          <Link to='/legal/privacy'>Privacy Policy</Link>
+          </div>
+
+          <div className="footer-link-terms">
+          <Link to='/legal/terms'>Terms of Use</Link>
+          </div>
+
           <div className="footer-copyright">
             Copyright © 2022 Community Cookbook. All rights reserved.
-          </div>     
+          </div>    
+           
         </footer>
     )
 
